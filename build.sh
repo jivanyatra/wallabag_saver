695bc3f59b40f2222c4900bbe529be28d39ef561
jivanyatra <debitkarma@gmail.com>
2025-09-21 23:56:28 -0400

    removed redundant and extranneous ActiveTab permission after chrome store rejection

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ set -e
 
 SRC_DIR="$(dirname "$0")"
 DIST_DIR="$SRC_DIR/dist"
+VERSION_CHROME=$(jq -r '.version' "$SRC_DIR/manifest.json")
+VERSION_FIREFOX=$(jq -r '.version' "$SRC_DIR/manifest_firefox.json")
 
 # Clean dist
 rm -rf "$DIST_DIR"
@@ -16,8 +18,7 @@ cp "$SRC_DIR"/manifest.json "$DIST_DIR/chrome/manifest.json"
 cp "$SRC_DIR"/*.js "$DIST_DIR/chrome/"
 cp "$SRC_DIR"/permissions_chrome.json "$DIST_DIR/chrome/permissions.json"
 cp "$SRC_DIR"/*.html "$DIST_DIR/chrome/"
-rm "$DIST_DIR/chrome/privacy_policy_firefox.html"
-mv "$DIST_DIR/chrome/privacy_policy_chrome.html" "$DIST_DIR/chrome/privacy_policy.html"
+cp "$SRC_DIR/docs/privacy_policy_chrome.html" "$DIST_DIR/chrome/privacy_policy.html"
 cp "$SRC_DIR"/*.css "$DIST_DIR/chrome/"
 cp "$SRC_DIR"/icon*.png "$DIST_DIR/chrome/"
 
@@ -26,14 +27,13 @@ cp "$SRC_DIR"/manifest_firefox.json "$DIST_DIR/firefox/manifest.json"
 cp "$SRC_DIR"/*.js "$DIST_DIR/firefox/"
 cp "$SRC_DIR"/permissions_firefox.json "$DIST_DIR/firefox/permissions.json"
 cp "$SRC_DIR"/*.html "$DIST_DIR/firefox/"
-rm "$DIST_DIR/firefox/privacy_policy_chrome.html"
-mv "$DIST_DIR/firefox/privacy_policy_firefox.html" "$DIST_DIR/firefox/privacy_policy.html"
+cp "$SRC_DIR/docs/privacy_policy_firefox.html" "$DIST_DIR/firefox/privacy_policy.html"
 cp "$SRC_DIR"/*.css "$DIST_DIR/firefox/"
 cp "$SRC_DIR"/icon*.png "$DIST_DIR/firefox/"
 
 # Zip builds
-cd "$DIST_DIR/chrome" && zip -r ../wallabag_saver_chrome.zip .
+cd "$DIST_DIR/chrome" && zip -r ../wallabag_saver_chrome_$VERSION_CHROME.zip .
 cd "../.."
-cd "$DIST_DIR/firefox" && zip -r ../wallabag_saver_firefox.zip .
+cd "$DIST_DIR/firefox" && zip -r ../wallabag_saver_firefox_$VERSION_FIREFOX.zip .
 
 echo "Build complete. Chrome and Firefox zips are in $DIST_DIR."
